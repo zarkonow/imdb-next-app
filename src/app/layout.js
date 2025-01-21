dad066764fc9f54ed48094dcbe5f60eceff438a5
zarkonow <stojanov1987@gmail.com>
2025-01-21 22:29:17 +0100

    Sync Clerk data to your app with webhooks

--- src/app/layout.js
+++ src/app/layout.js
@@ -2,7 +2,7 @@ import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
 import Header from "@/components/Header";
 import Providers from "@/components/ThemeCom";
-
+import { ClerkProvider } from "@clerk/nextjs";
 
 const geistSans = Geist({
   variable: "--font-geist-sans",
@@ -21,15 +21,17 @@ export const metadata = {
 
 export default function RootLayout({ children }) {
   return (
-    <html lang="en" suppressHydrationWarning>
-      <body
-        className={`${geistSans.variable} ${geistMono.variable} antialiased`}
-      >
-        <Providers>
-        <Header/>
-        {children}
-        </Providers>
-      </body>
-    </html>
+    <ClerkProvider>
+      <html lang="en" suppressHydrationWarning>
+        <body
+          className={`${geistSans.variable} ${geistMono.variable} antialiased`}
+        >
+          <Providers>
+            <Header />
+            {children}
+          </Providers>
+        </body>
+      </html>
+    </ClerkProvider>
   );
 }
--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,30 +1,35 @@
-import Link from 'next/link';
-import DarkModeSwitch from './DarkModeSwitch';
+import Link from "next/link";
+import DarkModeSwitch from "./DarkModeSwitch";
+import { SignedIn, SignedOut, UserButton } from "@clerk/nextjs";
 
 export default function Header() {
   return (
-    <div className='flex justify-between items-center p-3 max-w-6xlmx-auto'>
-        <ul className='flex gap-4'>
-            <li>
-                <Link href="/sing-in">Sing in</Link>
-            </li>
-            <li className='hidden sm:block'>
-                <Link href="/sing-in">Home</Link>
-            </li>
-            <li className='hidden sm:block'>
-                <Link href="/sing-in">About</Link>
-            </li>
-        </ul>
-        <div className='flex items-center gap-4'>
-<DarkModeSwitch />
-        
-        <Link href="/" className='flex gap-1 items-center'>
-        <span className='text-2xl font-bold bg-amber-500 py-1 px-2 rounded-lg'>
+    <div className="flex justify-between items-center p-3 max-w-6xlmx-auto">
+      <ul className="flex gap-4">
+        <SignedIn>
+          <UserButton />
+        </SignedIn>
+        <SignedOut>
+          <Link href={"/sign-in"}>Sign in</Link>
+        </SignedOut>
+
+        <li className="hidden sm:block">
+          <Link href="/">Home</Link>
+        </li>
+        <li className="hidden sm:block">
+          <Link href="/about">About</Link>
+        </li>
+      </ul>
+      <div className="flex items-center gap-4">
+        <DarkModeSwitch />
+
+        <Link href="/" className="flex gap-1 items-center">
+          <span className="text-2xl font-bold bg-amber-500 py-1 px-2 rounded-lg">
             IMDb
-        </span>
-        <span className='text-xl hidden sm:inline'>Clone</span>
+          </span>
+          <span className="text-xl hidden sm:inline">Clone</span>
         </Link>
-        </div>
+      </div>
     </div>
   );
 }

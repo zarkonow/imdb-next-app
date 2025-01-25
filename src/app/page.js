fa2dcc0d3b867db43516b20e6fb2ec1d9d55af95
zarkonow <stojanov1987@gmail.com>
2025-01-25 15:09:09 +0100

    Add loading state with spinner and refactor data fetching to include delay

--- src/app/page.js
+++ src/app/page.js
@@ -4,12 +4,21 @@ const API_KEY = process.env.API_KEY;
 
 export default async function Home({ searchParams }) {
   const genre = searchParams.genre || 'fetchTrending';
-  const res = await fetch(
-    `https://api.themoviedb.org/3${
-      genre === 'fetchTopRated' ? `/movie/top_rated` : `/trending/all/week`
-    }?api_key=${API_KEY}&language=en-US&page=1`,
-    { next: { revalidate: 10000 } }
-  );
+  
+  const res = await new Promise((resolve) =>{
+    setTimeout(async () => {
+    const response = await fetch(
+      `https://api.themoviedb.org/3${
+        genre === 'fetchTopRated' ? `/movie/top_rated` : `/trending/all/week`
+      }?api_key=${API_KEY}&language=en-US&page=1`,
+      { next: { revalidate: 3} }
+    );
+    resolve(response);
+  },2000);
+
+  })
+
+  
   const data = await res.json();
   if (!res.ok) {
     throw new Error('Failed to fetch data');

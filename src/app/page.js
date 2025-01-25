faeecc63be448dd13309e9688c7b70986b2b3f75
zarkonow <stojanov1987@gmail.com>
2025-01-25 14:43:56 +0100

    Refactor Home component to directly fetch and display results, removing FetchResults and Suspense

--- src/app/page.js
+++ src/app/page.js
@@ -1,32 +1,24 @@
-import { Suspense } from 'react';
 import Results from '@/components/Results';
 
 const API_KEY = process.env.API_KEY;
 
-async function FetchResults({ genre }) {
+export default async function Home({ searchParams }) {
+  const genre = searchParams.genre || 'fetchTrending';
   const res = await fetch(
     `https://api.themoviedb.org/3${
       genre === 'fetchTopRated' ? `/movie/top_rated` : `/trending/all/week`
     }?api_key=${API_KEY}&language=en-US&page=1`,
     { next: { revalidate: 10000 } }
   );
-
+  const data = await res.json();
   if (!res.ok) {
     throw new Error('Failed to fetch data');
   }
-
-  const data = await res.json();
-  return <Results results={data.results} />;
-}
-
-export default function Home({ searchParams }) {
-  const genre = searchParams.genre || 'fetchTrending';
+  const results = data.results;
 
   return (
     <div>
-      <Suspense fallback={<p>Loading...</p>}>
-        <FetchResults genre={genre} />
-      </Suspense>
+      <Results results={results} />
     </div>
   );
 }
